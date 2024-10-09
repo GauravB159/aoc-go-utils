2612a4e2ae95512094c759f60dc56956ffd6a5b0
Gaurav Bhagchandani <gaurav@supportinglines.com>
2024-10-08 22:12:27 -0400

    Fixed timer name

--- timer.go
+++ timer.go
@@ -5,10 +5,11 @@ import (
 	"time"
 )
 
-func TiemIt[T interface{}, R interface{}](name string, f func(T) R, arg T) R {
+func Timer[T interface{}, R interface{}](name string, f func(T) R, arg T) R {
 	start := time.Now()
 	result := f(arg)
 	fmt.Println("Answer:", result)
+	fmt.Println()
 	elapsed := time.Since(start)
 	fmt.Printf("%s took %s\n", name, elapsed)
 	return result
