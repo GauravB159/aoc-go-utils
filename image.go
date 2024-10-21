5d5172bdaf34c26b4d640c550c120ff24c77306d
Gaurav Bhagchandani <gaurav@supportinglines.com>
2024-10-21 00:01:03 -0400

    Moved stuff and added a palette method

--- image.go
+++ image.go
@@ -1,11 +1,40 @@
 package aocutils
 
-import "image"
+import (
+	"image"
+	"image/color"
+)
 
-func SetZoomedPixel(i int, j int, zoom int, image *image.Paletted, value int) {
-	for k := i * zoom; k < (i+1)*zoom; k++ {
-		for l := j * zoom; l < (j+1)*zoom; l++ {
-			image.Set(k, l, image.Palette[value])
+type Image struct {
+	raw  *image.Paletted
+	zoom int
+	rows int
+	cols int
+}
+
+func NewImage(rows int, cols int, zoom int) Image {
+	return Image{
+		raw:  image.NewPaletted(image.Rect(0, 0, cols*zoom, rows*zoom), make([]color.Color, 0, 0)),
+		rows: rows,
+		cols: cols,
+		zoom: zoom,
+	}
+}
+
+func (img *Image) UsePaletteReds() {
+	palette := make([]color.Color, 0, 11)
+	palette = append(palette, color.Black)
+	for i := 0; i <= 9; i++ {
+		palette = append(palette, color.RGBA{R: 60 + uint8(195*(float64(10-i)/10)), G: 0, B: 0, A: 255})
+	}
+	palette = append(palette, color.Transparent)
+	img.raw.Palette = palette
+}
+
+func (img *Image) SetZoomedPixel(i int, j int, value int) {
+	for k := i * img.zoom; k < (i+1)*img.zoom; k++ {
+		for l := j * img.zoom; l < (j+1)*img.zoom; l++ {
+			img.raw.Set(k, l, img.raw.Palette[value])
 		}
 	}
 }
