a904e65dcab5bc2d54aaecfd602accd3ea9b54a3
Gaurav Bhagchandani <gaurav@supportinglines.com>
2024-12-15 12:10:55 -0800

    Added separate zoom for rows and cols

--- image.go
+++ image.go
@@ -9,18 +9,20 @@ import (
 
 type Image struct {
 	raw      *image.Paletted
-	zoom     int
+	rowzoom  int
+	colzoom  int
 	rows     int
 	cols     int
 	filename string
 }
 
-func CreateImage(rows int, cols int, zoom int, filename string) Image {
+func CreateImage(rows int, cols int, rowzoom int, colzoom int, filename string) Image {
 	return Image{
-		raw:      image.NewPaletted(image.Rect(0, 0, cols*zoom, rows*zoom), make([]color.Color, 0, 0)),
+		raw:      image.NewPaletted(image.Rect(0, 0, cols*colzoom, rows*rowzoom), make([]color.Color, 0, 0)),
 		rows:     rows,
 		cols:     cols,
-		zoom:     zoom,
+		rowzoom:  rowzoom,
+		colzoom:  colzoom,
 		filename: filename + ".png",
 	}
 }
@@ -30,7 +32,7 @@ func (img *Image) GetRawImage() *image.Paletted {
 }
 
 func (img *Image) Clone() Image {
-	copied_image := CreateImage(img.rows, img.cols, img.zoom, img.filename)
+	copied_image := CreateImage(img.rows, img.cols, img.rowzoom, img.colzoom, img.filename)
 	copied_image.raw = image.NewPaletted(img.raw.Rect, img.raw.Palette)
 	copy(copied_image.raw.Pix, img.raw.Pix)
 	return copied_image
@@ -80,6 +82,22 @@ func (img *Image) UsePaletteRedToYellow() {
 	img.raw.Palette = palette
 }
 
+func (img *Image) UseFullColors() {
+	palette := make([]color.Color, 0, 10)
+	palette = append(palette, color.RGBA{R: 0, G: 18, B: 25, A: 255})     // 001219
+	palette = append(palette, color.RGBA{R: 0, G: 95, B: 115, A: 255})    // 005F73
+	palette = append(palette, color.RGBA{R: 10, G: 147, B: 150, A: 255})  // 0A9396
+	palette = append(palette, color.RGBA{R: 148, G: 210, B: 189, A: 255}) // 94D2BD
+	palette = append(palette, color.RGBA{R: 233, G: 216, B: 166, A: 255}) // E9D8A6
+	palette = append(palette, color.RGBA{R: 238, G: 155, B: 0, A: 255})   // EE9B00
+	palette = append(palette, color.RGBA{R: 202, G: 103, B: 2, A: 255})   // CA6702
+	palette = append(palette, color.RGBA{R: 187, G: 62, B: 3, A: 255})    // BB3E03
+	palette = append(palette, color.RGBA{R: 174, G: 32, B: 18, A: 255})   // AE2012
+	palette = append(palette, color.RGBA{R: 155, G: 34, B: 38, A: 255})   // 9B2226
+	palette = append(palette, color.RGBA{R: 0, G: 0, B: 0, A: 255})       // 000000
+	img.raw.Palette = palette
+}
+
 func (img *Image) UsePaletteColors() {
 	palette := make([]color.Color, 0, 7)
 	palette = append(palette, color.RGBA{R: 38, G: 70, B: 83, A: 255})
@@ -113,8 +131,8 @@ func (img *Image) UsePaletteGreens() {
 }
 
 func (img *Image) SetZoomedPixel(i int, j int, value int) {
-	for k := i * img.zoom; k < (i+1)*img.zoom; k++ {
-		for l := j * img.zoom; l < (j+1)*img.zoom; l++ {
+	for k := i * img.colzoom; k < (i+1)*img.colzoom; k++ {
+		for l := j * img.rowzoom; l < (j+1)*img.rowzoom; l++ {
 			img.raw.Set(k, l, img.raw.Palette[value])
 		}
 	}
